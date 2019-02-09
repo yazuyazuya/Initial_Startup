f0e6a4dc4d4ae334c1d55fb88bfabbd5705534c8
yazuyazuya <kzyono-1999@docomo.ne.jp>
2019-02-09 20:13:27 +0900

    Initial Commit

---
+++ Initial_StartupTests/Initial_StartupTests.swift
@@ -0,0 +1,34 @@
+//
+//  Initial_StartupTests.swift
+//  Initial_StartupTests
+//
+//  Created by 大野和也 on 2019/02/09.
+//  Copyright © 2019 yazuyazuya. All rights reserved.
+//
+
+import XCTest
+@testable import Initial_Startup
+
+class Initial_StartupTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
